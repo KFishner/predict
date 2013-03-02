0aedf0c9d4047d41f2f65bd1261d57341528df68
KFishner <KFishner@gmail.com>
2013-03-01 16:32:21 -0800

    fixed games typo

--- server.R
+++ server.R
@@ -8,7 +8,7 @@ shinyServer(function(input, output) {
   
   formulaPredict <- (reactive(function() {
     # session
-    session.genre <- ifelse(input$genre == "Total", as.numeric(1), ifelse(input$genre == "Games", as.numeric(.9), as.numeric(.05))) 
+    session.genre <- ifelse(input$genre == "Total", as.numeric(1), ifelse(input$genre == "games", as.numeric(.9), as.numeric(.05))) 
     session.country <- ifelse(input$country == "Total", as.numeric(9000000), ifelse(input$country == "US", as.numeric(1152945), ifelse(input$country == "GB", as.numeric(747938), ifelse(input$country == "JP", as.numeric(290220), error)))) 
     session.platform <- ifelse(input$platform == "Total", as.numeric(1), ifelse(input$platform == "ios", as.numeric(.9), as.numeric(.10))) 
     sessions <- session.country*session.genre*session.platform#*as.numeric(input$gender)*as.numeric(input$age)
@@ -44,7 +44,7 @@ shinyServer(function(input, output) {
   })
   
   formulaS <- reactive(function() {
-    session.genre <- ifelse(input$genre == "Total", as.numeric(1), ifelse(input$genre == "Games", as.numeric(.9), as.numeric(.05))) 
+    session.genre <- ifelse(input$genre == "Total", as.numeric(1), ifelse(input$genre == "games", as.numeric(.9), as.numeric(.05))) 
     session.country <- ifelse(input$country == "Total", as.numeric(9000000), ifelse(input$country == "US", as.numeric(1152945), ifelse(input$country == "GB", as.numeric(747938), ifelse(input$country == "JP", as.numeric(290220), error)))) 
     session.platform <- ifelse(input$platform == "Total", as.numeric(1), ifelse(input$platform == "ios", as.numeric(.9), as.numeric(.10))) 
     sessions.predicted <- session.country * session.genre * session.platform
